883e369ef22db34725fff55d46c397bfc5b033ac
bebra_dev <fivan999@yandex.ru>
2025-06-12 19:38:57 +0300

    chore: basic project structure

--- src/domain/dtos/booking.py
+++ src/domain/dtos/booking.py
@@ -1,21 +1,23 @@
-from pydantic import BaseModel, EmailStr, Field, model_validator
-from typing_extensions import Self
 from datetime import datetime
 
+from pydantic import BaseModel, Field, model_validator
+from typing_extensions import Self
 
-class BookingDTO(BaseModel):
-    email: EmailStr = Field(
-        ..., description="The email of the person making the reservation"
-    )
-    title: str = Field("", max_length=40, description="Booking title")
-    room: str = Field(
-        ..., max_length=10, description="Room number for reservation"
-    )
+
+class BaseBookingDTO(BaseModel):
     start: datetime = Field(..., description="Start time of room reservation")
     end: datetime = Field(..., description="End time of room reservation")
+    room: str = Field(..., max_length=10, description="Room for reservation")
 
     @model_validator(mode="after")
     def validate_date(self) -> Self:
         if not self.start < self.end:
             raise ValueError("Start time has to be less than end time")
         return self
+
+
+class BookingWithTeacherAndGroup(BaseBookingDTO):
+    teacher: str = Field(..., max_length=50, description="Teacher on lesson")
+    group_name: str = Field(
+        ..., max_length=10, description="Name of the group"
+    )
