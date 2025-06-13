830bea65996eb3f72e0d67124c306143570b9632
wannebetheshy <74280345+wannebetheshy@users.noreply.github.com>
2025-06-13 21:49:57 +0300

    chore: teacher assistants fix

--- src/presentation/dependencies/taprovider.py
+++ src/presentation/dependencies/taprovider.py
@@ -1,16 +1,21 @@
 from dishka import Provider, Scope, provide
 
-from src.domain.interfaces.teacher_assistant_manager import (
-    ITeacherAssistantManager,
-)
-from src.application.use_cases.teacher_assistant_manager import (
-    TeacherAssistantManager,
-)
+from src.domain.dtos.teacher import TeacherDTO
+
+from yaml import safe_load
 
 
 class TeacherAssistantManagerProvider(Provider):
     scope = Scope.APP
 
     @provide
-    def get_manager(self) -> ITeacherAssistantManager:
-        return TeacherAssistantManager()
+    def get_teachers(self, file_path: str) -> list[TeacherDTO]:
+        assistants = list()
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = safe_load(file)
+            for teacher in data["staff"]:
+                assistant = TeacherDTO(
+                    name=teacher["name"], group=teacher["group"]
+                )
+                assistants.append(assistant)
+        return assistants
